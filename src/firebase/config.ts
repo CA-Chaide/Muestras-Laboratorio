8ce6a609f60e66ca68f764b1b40bfcfa906fc6f5
TI-Chaide <tecnologia@chaideychaide.com>
2026-03-24 09:20:32 -0500

    CA: Borrado de dependencias

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-2084456687-65655",
-  "appId": "1:739889392609:web:f39b8a5fa1f7b1d86bc0a0",
-  "apiKey": "AIzaSyDYhKgOeOQcoVcZtHfviNi9iBzwGYd1By8",
-  "authDomain": "studio-2084456687-65655.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "739889392609"
-};
