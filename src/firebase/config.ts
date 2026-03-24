08f678dee44fff9facbe3b40144242430873be85
Cristhian Andrade <cristhian.andrade@chaideychaide.com>
2026-03-24 14:03:56 +0000

    Primer envío de mi proyecto Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2084456687-65655",
   "appId": "1:739889392609:web:f39b8a5fa1f7b1d86bc0a0",
-  "apiKey": "AIzaSyCxZg78QDYRNEduQv_pW88ndUosdCr5KDA",
+  "apiKey": "AIzaSyDYhKgOeOQcoVcZtHfviNi9iBzwGYd1By8",
   "authDomain": "studio-2084456687-65655.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "739889392609"
